579fa649c30ec8c18f8a56fda8f5340cd6df78db
lerrybe <lerry597@gmail.com>
2022-06-09 02:19:08 +0900

    feat: implement animations of App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,48 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const rotationAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50% {
+    border-radius:100px;
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 48px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
+
 export default function App() {
-  return null;
+  return (
+    <Wrapper>
+      <Box>
+        <span>🎃</span>
+      </Box>
+    </Wrapper>
+  );
 }
